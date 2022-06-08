55f4854279b31342c5f3ed48729f6d0a78c296fa
Aleeq <artemchuki@gmail.com>
2022-06-08 20:04:44 +0300

    Appes

---
+++ src/com/company/Main35.java
@@ -0,0 +1,26 @@
+package com.company;
+
+enum level{
+    Low,
+    Medium,
+    Hard
+}
+
+public class Main35 {
+    public static void main(String[] args) {
+        level var = level.Hard;
+
+        switch (var){
+            case Low:
+                System.out.println("Level is Low");
+                break;
+            case Medium:
+                System.out.println("Level is Medium");
+                break;
+            case Hard:
+                System.out.println("Level is Hard");
+                break;
+        }
+
+    }
+}
